6405291ab9ad3435752418c81b67034b27783b87
saleszera <crowzcoleslaw@gmail.com>
2021-02-22 19:42:22 -0300

    configurando eslint

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.status(200).json({ok: "hello"})
-})
+  return response.status(200).json({ ok: 'hello' });
+});
 
- app.listen(3333, () => console.log("Server is running"));
+app.listen(3333, () => console.log('Server is running'));
